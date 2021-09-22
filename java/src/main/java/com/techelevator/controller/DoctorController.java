bc299ddfce493d24096cc9c267b1c98a6165add4
James Caserta <jcaserta90@gmail.com>
2021-09-22 12:30:46 -0400

    started Controllers

--- java/src/main/java/com/techelevator/controller/DoctorController.java
+++ java/src/main/java/com/techelevator/controller/DoctorController.java
@@ -1,22 +1,24 @@
 package com.techelevator.controller;
 
+
 import com.techelevator.dao.UserDao;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@PreAuthorize("isAuthenticated()")
-@RequestMapping("patient")
 @RestController
-public class PatientController {
+@PreAuthorize("isAuthenticated()")
+@RequestMapping("doctor")
+public class DoctorController {
 
-    //private PatientDao patientDao;
+    //private DoctorDao doctorDao;
     private UserDao userDao;
 
 
     @RequestMapping(path = "", method = RequestMethod.GET)
-    public long getPatient(){
+    public long getDoctor(){
         return 0;
     }
+
 }
