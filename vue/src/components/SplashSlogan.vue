55de1b207c85dcd602054f6f12efd8430502ad0c
AJ Page <lapage3@gmail.com>
2021-09-23 17:03:24 -0400

    9/23 frontend

--- vue/src/components/SplashSlogan.vue
+++ vue/src/components/SplashSlogan.vue
@@ -1,8 +1,7 @@
 <template>
     <div id="splashslogan">
 
-    <span style="color:#07329E" id="title1">DrSched</span>
-    <span style="color:#07329E" id="title2">We make booking with your doctor easy.</span>
+    <h2><span style="color:#07329E" id="title2">We make booking with your doctor easy.</span></h2>
     <span style="color:#07329E" id="title3">Find your doctor and book today.</span>
 
     </div>
